98c1e94ff6fd9330bdd0ec3c0fb2e338e39f88b9
Маргарита Фирсова <Margaret@MacBook-Pro-Margarita.local>
2019-02-14 12:11:09 +0000

    added nav bar; games and books bones; finished life bones

---
+++ gamesgallery.js
@@ -0,0 +1,23 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Thumbnail image controls
+function currentphilslide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("philslide");
+  var dots = document.getElementsByClassName("demo");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+ 
+}
